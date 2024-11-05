181f8ad28a63541fdff2d337462cd099304c584b
linlinh75 <thuylinhnguyenat@gmail.com>
2024-11-05 05:48:12 +0000

    pull

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -11,7 +11,7 @@ public class DBContext {
         String url = "jdbc:mysql://localhost:3306/swp"; 
         String user = "root"; 
 
-        String password = "nguyetanh2311"; 
+        String password = "1234"; 
 
         
         try {
